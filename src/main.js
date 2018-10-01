7e8e1535d589bb0709da2fcf5940041a254378f4
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-01 20:36:23 -0300

    main: remove vue-apollo generated config

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
