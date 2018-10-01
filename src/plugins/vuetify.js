a7d7087166f5309e05b5ac8f409d1cfa9f0bb754
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-01 20:44:49 -0300

    vuetify: add color theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import pt from 'vuetify/es5/locale/pt'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   iconfont: 'fa',
   lang: {
     locales: { pt },
     current: 'pt'
+  },
+  theme: {
+    primary: colors.blue.darken4,
+    secondary: colors.lightBlue.lighten2,
+    tertiary: colors.blueGrey.darken2
   }
 })
