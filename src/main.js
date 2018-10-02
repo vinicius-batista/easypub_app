a7cc6913c08ec4b8bfefa7788674a58c05c6d498
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-02 10:19:27 -0300

    services: move router and store to services

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import { router, store } from './services'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
