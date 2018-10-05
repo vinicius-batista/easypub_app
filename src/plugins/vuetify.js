010d06a8b4000829181d6a76b5c0a6515a5b6fb7
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-05 20:15:07 -0300

    font: change font to ubuntu

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import '@/assets/styles/main.styl'
 import pt from 'vuetify/es5/locale/pt'
 import colors from 'vuetify/es5/util/colors'
 
