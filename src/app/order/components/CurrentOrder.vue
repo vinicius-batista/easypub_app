400a71a72caf7831cfe9dce3c614d5389dffd100
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-13 10:57:56 -0300

    order: add closeOrder integration

--- src/app/order/components/CurrentOrder.vue
+++ src/app/order/components/CurrentOrder.vue
@@ -9,7 +9,7 @@
           :query="$options.currentOrderQuery"
         >
           <template slot-scope="{ result: { data } }">
-            <div v-if="data">
+            <div v-if="data && data.currentOrder">
               <v-card-text class="text-xs-center">
                 <h6 class="subheading grey--text text--darken-3 font-weight-medium">
                  {{ calculateTotal(data.currentOrder.items) | formatMoney }}
@@ -33,30 +33,56 @@
                   </v-list-tile>
                 </v-list>
               </v-card-text>
+              <ApolloMutation
+                :mutation="$options.closeOrderMutation"
+                :variables="{ orderId: data.currentOrder.id }"
+                :update="updateStore"
+              >
+                <template slot-scope="{ mutate, loading }">
+                  <v-card-actions class="pa-4">
+                    <v-btn color="primary" block v-bind="{ loading }" @click="mutate()">
+                      FINALIZAR PEDIDO
+                    </v-btn>
+                  </v-card-actions>
+                </template>
+              </ApolloMutation>
+            </div>
+            <div v-else>
+              <v-card-text class="text-xs-center pa-4">
+                <h5 class="grey--text text--darken-2 subheading">Você não possui um pedido em andamento</h5>
+              </v-card-text>
             </div>
           </template>
         </ApolloQuery>
-        <v-card-actions class="pa-4">
-          <v-btn color="primary" block>FINALIZAR PEDIDO</v-btn>
-        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import { currentOrderQuery } from '@/domains/order/graphql'
+import { currentOrderQuery, closeOrderMutation } from '@/domains/order/graphql'
 import { path, sum, map, pipe } from 'ramda'
+import { mapMutations } from 'vuex'
 
 export default {
   name: 'CurrentOrder',
   currentOrderQuery,
+  closeOrderMutation,
   methods: {
+    ...mapMutations('order', ['setTableId']),
     calculateTotal (items) {
       return pipe(
         map(path(['menuItem', 'price'])),
         sum
       )(items)
+    },
+    updateStore (store) {
+      const { currentOrderQuery } = this.$options
+      store.writeQuery({
+        query: currentOrderQuery,
+        data: { currentOrder: null }
+      })
+      this.setTableId('')
     }
   }
 }
--- src/domains/order/graphql/index.js
+++ src/domains/order/graphql/index.js
@@ -1,2 +1,3 @@
 export { default as addItemToOrderMutation } from './mutations/addItemToOrder.gql'
 export { default as currentOrderQuery } from './queries/currentOrder.gql'
+export { default as closeOrderMutation } from './mutations/closeOrder.gql'
