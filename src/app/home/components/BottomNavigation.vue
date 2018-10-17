9d699e393a6e09b39e1d8398606c44b33cb4e9cb
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-17 16:08:26 -0300

    home: fix performance issues in bottom navigation

--- src/app/home/components/BottomNavigation.vue
+++ src/app/home/components/BottomNavigation.vue
@@ -1,22 +1,19 @@
 <template>
-  <v-footer app height="56">
-    <v-bottom-nav
-      :active.sync="activeBtn"
-      absolute
-      value="true"
-      color="white"
+  <v-footer app height="56" class="white elevation-4">
+    <v-btn
+      v-for="{text, icon, to} in buttons"
+      :key="text"
+      class="grey--text nav-button-group"
+      active-class="primary--text"
+      flat
+      :to="to"
+      block
     >
-      <v-btn
-        v-for="{text, icon, to} in buttons"
-        :key="text"
-        color="primary"
-        flat
-        :to="to"
-      >
+      <div class="nav-button-group__items font-weight-regular">
         {{text}}
         <v-icon>{{icon}}</v-icon>
-      </v-btn>
-    </v-bottom-nav>
+      </div>
+    </v-btn>
   </v-footer>
 </template>
 
@@ -45,3 +42,14 @@ export default {
   })
 }
 </script>
+
+<style scoped>
+.nav-button-group {
+  padding: 2em 0em
+}
+.nav-button-group__items {
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: center;
+}
+</style>
