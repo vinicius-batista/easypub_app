245cace9f44d71afa0f43254b1499dd822510993
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-11-01 11:14:41 -0300

    bar: change bar detail elements

--- src/app/bar/components/BarDetail.vue
+++ src/app/bar/components/BarDetail.vue
@@ -2,19 +2,20 @@
   <v-layout wrap>
     <v-flex xs12>
       <v-card flat>
-        <v-layout row align-center>
-          <v-flex xs7>
+        <v-layout row align-center wrap>
+          <v-flex xs12 justify-center>
+            <v-img :src="avatar" height="auto" contain aspect-ratio="2.75"/>
+          </v-flex>
+          <!-- <v-flex xs7>
             <v-card-title primary-title>
               <h5 class="headline tertiary--text font-weight-medium">{{ name }}</h5>
-              <h6 class="tertiary--text subheading py-2">{{ address }}</h6>
+
             </v-card-title>
-          </v-flex>
-          <v-flex xs5>
-            <v-img :src="avatar" height="80px" contain />
-          </v-flex>
+          </v-flex> -->
         </v-layout>
-        <v-card-actions>
+        <v-card-actions class="py-3">
           <BarStatusChip :status="status" />
+          <h6 class="tertiary--text subheading py-2 ml-2">{{ address }}</h6>
         </v-card-actions>
       </v-card>
     </v-flex>
